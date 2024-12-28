7f78127481160055db86f72a857b41f772176e63
Swadeep <swadeep72@gmail.com>
2024-12-29 01:59:01 +0530

    Added error handling in auth middleware.

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,18 +2,14 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import { TryCatch } from "../utils/helperUtils.js";
 
-const userAuth = TryCatch(async (req, res, next) => {
+const userAuth = TryCatch(async (req, _, next) => {
     const { token } = req.cookies;
     console.log(token)
-    if (!token) {
-        throw new Error("Token is invalid.")
-    }
+    if (!token) return next([401, "Token is invalid."])
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
     const { id } = decodedToken;
     const user = await User.findById(id)
-    if (!user) {
-        throw new Error("User not found.")
-    }
+    if (!user) return next([401, "User not found."])
     req.user = user
     next()
 })
