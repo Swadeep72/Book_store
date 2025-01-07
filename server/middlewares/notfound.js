e8ee26830082a881a5d8c953c2cbf1b072ad1ad8
Swadeep <swadeep72@gmail.com>
2025-01-07 11:30:14 +0530

    Updated middleware when route isn't available to send the error precisely.

--- server/middlewares/notfound.js
+++ server/middlewares/notfound.js
@@ -1,6 +1,17 @@
-import { NOT_FOUND } from "../utils/helperUtils.js";
+import bookRoutes from "../routes/bookRoutes.js";
+import cartRoutes from "../routes/cartRoutes.js";
+import favouriteRoutes from "../routes/favouriteRoutes.js";
+import orderRoutes from "../routes/orderRoutes.js";
+import userRoutes from "../routes/userRoutes.js";
+import { NOT_FOUND, printAllRoutes } from "../utils/helperUtils.js";
 
-export default function (req, res, next) {
-    next([NOT_FOUND, "Route not found."]);  // Either use next or use res.send both will work fine.
+export default function (req, _, next) {
+    const allRoutes = [userRoutes, bookRoutes, cartRoutes, favouriteRoutes, orderRoutes]
+        ?.map(router => printAllRoutes(router))?.flat(2)
+        ?.map(route => route?.split(": ")?.[0])
+    const isExists = allRoutes?.some(data => data === req?.originalUrl)
+
+    // next([NOT_FOUND, isExists ? "Route not found but path exists." : "Route not found."]);
+    next([NOT_FOUND, `Route not found${isExists ?" but path exists.":"."}`]);
     // res.status(NOT_FOUND).json({ status: 0, message: "Route not found" })
 }
